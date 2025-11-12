8e4f0ec9f4af6b4d2dbad76abdb2310707289466
Coda <69929239+Coda9X@users.noreply.github.com>
2025-11-12 22:10:18 +0330

    Add files via upload
    v0.0.2

---
+++ config/socials.php
@@ -0,0 +1,37 @@
+<?php
+
+return [
+    [
+        "slug"         => "telegram",
+        "url"          => "https://coda_9x.t.me",
+        "image"        => "assets/img/0xpfp/0xp1.jpg",
+        "name"         => "[ C‌‌‌‌‌r‌‌‌e‌‌‌‌a‌‌‌‌‌‌‌‌‌m‌‌‌‌‌C‌‌‌‌‌‌h‌‌‌‌‌‌‌e‌‌e‌s‌‌‌‌‌e ]",
+        "p"            => "Half of C",
+        "app_icon"     => "assets/svg/0xs4.svg",
+        "cta_icon"     => "assets/svg/0xi1.svg",
+        "app"          => "Telegram",
+        "published_at" => "2025-10-27",
+    ],
+    [
+        "slug"         => "instagram",
+        "url"          => "https://www.instagram.com/_.itscoda/",
+        "image"        => "assets/img/0xpfp/0xp2.jpg",
+        "name"         => "_.itscoda",
+        "p"            => "I don't write code",
+        "app_icon"     => "assets/svg/0xs3.svg",
+        "cta_icon"     => "assets/svg/0xi1.svg",
+        "app"          => "Instagram",
+        "published_at" => "2025-10-27",
+    ],
+    [
+        "slug"         => "github",
+        "url"          => "https://github.com/Coda9X",
+        "image"        => "assets/img/0xpfp/0xp3.jpg",
+        "name"         => "Coda9X",
+        "p"            => "I don't write code",
+        "app_icon"     => "assets/svg/0xs2.svg",
+        "cta_icon"     => "assets/svg/0xi1.svg",
+        "app"          => "Github",
+        "published_at" => "2025-10-27",
+    ],
+];
